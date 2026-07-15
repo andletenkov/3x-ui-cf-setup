d33b1c6e05a52dddc3c052a943c6f05469fed41f
Andrei Letenkov <andletenkov@gmail.com>
2026-07-15 19:13:52 +0200

    fix: rewrite SUB_PATH to /sub/ when proxying to 3x-ui subscription backend

--- setup_nginx_proxy.sh
+++ setup_nginx_proxy.sh
@@ -720,7 +720,7 @@ server {
     }
 
     location ${SUB_PATH}/ {
-        proxy_pass http://127.0.0.1:${SUB_PORT};
+        proxy_pass http://127.0.0.1:${SUB_PORT}/sub/;
 
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
